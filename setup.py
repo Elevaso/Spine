ddb70b259a003cb004aa5460e1fbe1f882ce618d
Andrew Rau <arau@elevaso.org>
2024-03-20 15:23:51 -0500

    Fixing packaging code

--- setup.py
+++ setup.py
@@ -1,18 +1,11 @@
 #!/usr/bin/env python
 
 import os.path
-from setuptools import setup, find_packages
+from setuptools import setup
 import sys
 
 ROOT = os.path.abspath(os.path.dirname(__file__))
 
 sys.path.insert(0, os.path.join(ROOT))
 
-setup(
-    package_dir={"": "src"},
-    packages=find_packages(
-        "src", exclude=["tests*", "scripts*", "docs*", "tools*"]
-    ),
-    include_package_data=True,
-    zip_safe=True,
-)
+setup()
